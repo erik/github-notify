2f7505310e20c4c9291c828002c3dfc0470d72b5
Erik Price <github@erikprice.net>
2017-09-18 21:07:01 -0700

    Initial Commit

---
+++ GithubNotify/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GithubNotify
+//
+//  Created by Erik Price on 9/18/17.
+//  Copyright © 2017 Erik Price. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GithubNotify/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GithubNotify
+//
+//  Created by Erik Price on 9/18/17.
+//  Copyright © 2017 Erik Price. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
