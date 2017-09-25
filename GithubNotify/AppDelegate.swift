d00930d9cde1ba325f35e94e1eacc63918167349
Erik Price <github@erikprice.net>
2017-09-24 19:16:22 -0700

    Reimplement everything with oauth2 lib

--- GithubNotify/AppDelegate.swift
+++ GithubNotify/AppDelegate.swift
@@ -1,45 +1,94 @@
-
 import Cocoa
+import OAuth2
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    let STATUS_ITEM = NSStatusBar.system().statusItem(withLength: NSSquareStatusItemLength)
-
+    var github: GithubLoader!
+    var unreadCount = 0
+    let statusItem = NSStatusBar.system().statusItem(withLength: NSSquareStatusItemLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        NSAppleEventManager.shared().setEventHandler(self, andSelector: #selector(AppDelegate.handleGetURL(event:withReplyEvent:)), forEventClass: AEEventClass(kInternetEventClass), andEventID: AEEventID(kAEGetURL))
+        // Register our URL scheme.
+        NSAppleEventManager.shared().setEventHandler(
+            self,
+            andSelector: #selector(AppDelegate.handleGetURL(event:withReplyEvent:)),
+            forEventClass: AEEventClass(kInternetEventClass),
+            andEventID: AEEventID(kAEGetURL))
 
-        if let button = STATUS_ITEM.button {
-            button.image = NSImage(named: "MenuIconDefault")
-            button.action = #selector(togglePopover(_:))
+        self.unreadCount = 0
+
+        // Pull Github OAuth credentials
+        guard let infoPlist = Bundle.main.infoDictionary,
+            let clientId = infoPlist["GITHUB_OAUTH_CLIENT_ID"] as? String,
+            let clientSecret = infoPlist["GITHUB_OAUTH_CLIENT_SECRET"] as? String else {
+                print("Missing GitHub credentials in Info.plist!")
+                return
         }
 
-        InitGithub()
-        Timer.scheduledTimer(timeInterval: 60.0, target: self, selector: #selector(AppDelegate.refresh), userInfo: nil, repeats: true)
+        self.github = GithubLoader(clientId: clientId, clientSecret: clientSecret)
+        self.github.attemptToAuthorize(callback: { _, error in
+            if error != nil {
+                NSAlert(error: error!).runModal()
+                NSApp.terminate(self)
+            } else {
+                self.refreshNotifications()
+            }
+        })
 
-    }
+        self.updateMenuIcon()
 
-    
-    func applicationWillTerminate(_ aNotification: Notification) {
+        // Refresh the unread notification count.
+        Timer.scheduledTimer(timeInterval: 60.0,
+                             target: self,
+                             selector: #selector(AppDelegate.refreshNotifications),
+                             userInfo: nil,
+                             repeats: true)
     }
 
     @objc
-    func togglePopover(_ sender: Any?) {
+    func openNotificationUrl(_ sender: Any?) {
         NSWorkspace.shared().open(URL(string: "https://github.com/notifications")!)
     }
 
-    func refresh() {
+    func updateMenuIcon() {
+        let icon : String
+        if self.unreadCount > 0 {
+            icon = "MenuIconUnread"
+        } else {
+            icon = "MenuIconDefault"
+        }
+
+        if let button = statusItem.button {
+            button.toolTip = "\(self.unreadCount) unread notifications."
+            button.image = NSImage(named: icon)
+            button.action = #selector(openNotificationUrl(_:))
+        }
+    }
+
+    func refreshNotifications() {
         print("refreshing!")
-        updateNotifications()
+        self.github.refreshNotifications() { notifications, error in
+            if let error = error {
+                NSAlert(error: error).runModal()
+                return
+            }
+
+            self.unreadCount = notifications!.count
+            self.updateMenuIcon()
+        }
     }
 
     func handleGetURL(event: NSAppleEventDescriptor!, withReplyEvent: NSAppleEventDescriptor!) {
         if let urlString = event.paramDescriptor(forKeyword: AEKeyword(keyDirectObject))?.stringValue,
-            let url = URL(string: urlString) {
+            let url = URL(string: urlString)  {
 
             if url.scheme == "github-notify" {
-                HandleGithubOAuthURL(url: url)
+                do {
+                    try self.github.oauth2.handleRedirectURL(url)
+                } catch let error {
+                    NSAlert(error: error).runModal()
+                }
             }
 
         }
--- GithubNotify/GithubLoader.swift
+++ GithubNotify/GithubLoader.swift
@@ -1,5 +1,47 @@
 import Foundation
+import OAuth2
+import SwiftyJSON
 
-class GithubLoader {
+class GithubLoader: OAuth2DataLoader {
+    let baseURL = URL(string: "https://api.github.com/")!
 
+    public init(clientId: String, clientSecret: String) {
+        let oauth = OAuth2CodeGrant(settings: [
+            "client_id": clientId,
+            "client_secret": clientSecret,
+            "authorize_uri": "https://github.com/login/oauth/authorize",
+            "token_uri": "https://github.com/login/oauth/access_token",
+            "scope": "notifications",
+            "redirect_uris": ["github-notify://oauth"],
+            "secret_in_body": true,
+            "verbose": true,
+        ])
+
+        super.init(oauth2: oauth)
+    }
+
+    public func refreshNotifications(callback: @escaping (([JSON]?, Error?) -> Void)) {
+        oauth2.logger = OAuth2DebugLogger(.trace)
+
+        let url = baseURL.appendingPathComponent("notifications")
+
+        var request = oauth2.request(forURL: url)
+        request.setValue("application/vnd.github.v3+json", forHTTPHeaderField: "Accept")
+
+        perform(request: request) { response in
+            do {
+                let responseString = String(data: try response.responseData(), encoding: .utf8)!
+                let notifications = JSON.parse(responseString).array!
+
+                DispatchQueue.main.async() {
+                    callback(notifications, nil)
+                }
+            }
+            catch let error {
+                DispatchQueue.main.async() {
+                    callback(nil, error)
+                }
+            }
+        }
+    }
 }
